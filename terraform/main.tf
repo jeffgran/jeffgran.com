8cf3cffed3e729b5cfd6d9892c647bd04137ad47
Jeff Gran <jeff.gran@gmail.com>
2020-07-01 19:53:02 -0600

    import route53 zone and records

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,27 @@ provider "aws" {
   region = "us-west-1"
 }
 
+resource "aws_route53_zone" "jeffgran-com" {
+  name = "jeffgran.com"
+}
+
+resource "aws_route53_record" "naked" {
+  zone_id = aws_route53_zone.jeffgran-com.zone_id
+  name = "jeffgran.com."
+  type = "A"
+  ttl = "300"
+  records = ["162.241.226.160"]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.jeffgran-com.zone_id
+  name = "www.jeffgran.com."
+  type = "CNAME"
+  ttl = "300"
+  records = ["jeffgran.com"]
+}
+
+
 
 module "website" {
   source    = "git::https://github.com/cloudposse/terraform-aws-s3-website.git?ref=tags/0.9.0"
